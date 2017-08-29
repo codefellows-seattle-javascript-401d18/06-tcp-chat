e5f19889bb75f732c110c03283274f46a376ec64
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-08-28 17:44:46 -0700

    I have one thing workinggit add .

--- lab-michelle/server.js
+++ lab-michelle/server.js
@@ -1,14 +1,14 @@
 'use strict'
 
 // TODO: require in all the modules necessary for server setup
-const net = require('net')
-const EE = require('events').EventEmitter
-const ee = new EE()
-const Client = require('./model/client')
+const net = require('net');
+const EE = require('events').EventEmitter;
+const ee = new EE();
+const Client = require('./model/client');
 
-const server = net.createServer()
+const server = net.createServer();
 
-let pool = []
+let pool = [];
 
 // TODO: Create the @dm, @nick, @all commands. Anything else should emit a 'default' event
 // @all HEY THERE => this will broadcast to all clients
@@ -32,16 +32,25 @@ server.on('connection', socket => {
       console.log(data.toString().split(' ').slice(1).join() + '\n');
       pool.forEach(c => c.socket.write(data.toString().split(' ').slice(1).join() + '\n'));
     } else if(cmd === '@nick') {
-  
+      let makeName = data.toString().split('').pop().trim();
+      client.name = makeName;
+      // console.log(client.name);
+      pool.forEach(c => c.socket.write(`${client.nick} changed his name!`));
+      // client.socket.write(`You changed your name! Now it is ${string.split(' ', 1)}\n`);
     } else if(cmd === '@dm') {
-
+      let messageArr = data.toString().split(' ');
+      for (let i = 0; i < pool.length; i++) {
+        if (client.nick === messageArr[1]) {
+          socket.write(messageArr[2]);
+        }
+      }
     }
     // console.log(data.toString()) => will print whatever data was transmitted
   });
 
-  socket.error()
-
-  socket.close()
+  // socket.error();
+  //
+  // socket.close();
 });
 
 server.listen(3000, () => console.log('listening on port 3000'));
