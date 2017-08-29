e18cd6de02b4d27729296c39fa777e04fd15faa0
kpryzk <kepryzk@gmail.com>
2017-08-28 19:01:39 -0700

    oops forgot to acp until i was done

--- lab-katherine/model/client.js
+++ lab-katherine/model/client.js
@@ -1,15 +1,9 @@
-//require in uuid
-//we will take advantage of the net module in node - we will also use some event emitters
-//uuid is an almost completely unique id
-
-
 'use strict'
+
 const uuid = require('uuid/v4')
 
-//TODO: Client needs a nickname, _id, and socket - this is what the server will hand off
-
-model.exports = function(socket){
+module.exports = function(socket) {
   this.socket = socket
-  this.neck = `guest_${Math.random() * 10000}`
+  this.nick = `guest_${Math.random() * 10000}`
   this._id = uuid()
 }
--- lab-katherine/server.js
+++ lab-katherine/server.js
@@ -1,5 +1,5 @@
-//require in all the modules for server setup =
 'use strict'
+
 const net = require('net')
 const EE = require('events').EventEmitter
 const ee = new EE()
@@ -9,37 +9,50 @@ const server = net.createServer()
 
 let pool = []
 
-//TODO create the @dm, @nick, @all commands anything else should emit a default
-
 ee.on('default', (client, string) => {
-  //if i type a comman and transmit hello world you haven't specified who you are addressing, so this will catch all improperly formatted commands
-  client.socket.write(`improperly formatted command: ${string.split(' ', 1)}\n`)
+  client.socket.write(`Improperly formatted command: ${string.split(' ', 1)}\n`)
 })
 
 server.on('connection', socket => {
-  //the server is going to hear the connection, create a socket, and pass that socket to the callback - the socket it our connection to the server
   let client = new Client(socket)
   pool.push(client)
-  //iterate over the client pool and write to everyone's socket - this allows us both read and write access
-  //can broadcast to everyone by writing to their socket
   pool.forEach(c => c.socket.write(`${client.nick} has joined the channel\n`))
 
   socket.on('data', data => {
-    //when data is transported, it is done in the form of a buffer
-    // data.toString() => will need to print whatever data was transmitted
     let cmd = data.toString().split(' ').shift().trim()
-    if(cmd === '@all'){
-      // pool.forEach(c => c.socket.write(data.toString()))
+    console.log(cmd)
 
-    } else if (cmd === '@dm'){
+    if(cmd === '@all') {
+      pool.forEach(c => c.socket.write(`${client.nick}: ${data.toString()}\n`))
 
+    } else if(cmd === '@nick') {
+      let aGuest = data.toString().split(' ').slice(1,2).join().trim();
+      pool.forEach(c => c.socket.write(`${client.nick} changed their name to ${aGuest}\n`))
+      client.nick = aGuest
+
+    } else if(cmd === '@dm') {
+      let aGuest = data.toString().split(' ').slice(1,2).join();
+      pool.forEach(function(c) {
+        if (c.nick === aGuest){
+          c.socket.write(`${client.nick}: ${data.toString()}\n`)
+        }
+      })
     } else {
-    ee.emit('default', client, data.toString().split(' ').slice(1).join())
-  }
+      ee.emit('default', client, data.toString().split(' ').slice(1).join())
+    }
+  })
+  socket.on('error', error =>{
+    console.log(error)
+  })
+  socket.on('close', close => {
+    var index = pool.indexOf(client)
+    if (index > -1) {
+      pool.splice(index, 1);
+      console.log('pool', pool)
+    }
+    pool.forEach(c => c.socket.write(`${client.nick} has left the channel\n`))
   })
 })
 
-//to start a server mybe nc localhost 3000
-//man nc
-//telnet localhost:3000
-server.listen(3000, () => console.log(`listening on port 3000`))
+
+server.listen(3000, () => console.log('listening on port 3000'))
