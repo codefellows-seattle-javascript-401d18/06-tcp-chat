c391a333e386434e818267b44014dbc57645b3ec
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-09-02 19:55:09 -0700

    Updated so ees and methods _should_ work

--- lab-michelle/server.js
+++ lab-michelle/server.js
@@ -1,72 +1,50 @@
 'use strict';
 
-// TODO: require in all the modules necessary for server setup
 const net = require('net');
 const EE = require('events').EventEmitter;
 const ee = new EE();
 const Client = require('./model/client');
+// const cmd = require('./lib/command-parser');
+const PORT = process.env.PORT || 3000;
 
 const server = net.createServer();
 
 let pool = [];
 
-// TODO: Create the @dm, @nick, @all commands. Anything else should emit a 'default' event
-// @all HEY THERE => this will broadcast to all clients
-// @dm tim HEY THERE => this will broadcast to ONLY tim
-// @nick scott => will change the nickname of this client to scott
-
+//The events we will be emitting
 ee.on('default', (client, string) => {
   client.socket.write(`Improperly formatted command: ${string.split(' ', 1)}\n`);
 });
 
-//Node docs suggest trying to catch uncaught exceptions through this type of thing? https://nodejs.org/api/events.html#events_class_eventemitter
-process.on('error', (err) => {
-  console.error('Darn, an error occured');
+ee.on('@all', (client, string) => pool.forEach(c => c.socket.write(`${client.nick} has joined the channel\n`)));
+
+ee.on('@nick', (client, newName) => {
+  pool.filter(c=> c === client)[0].nick = newName;
+  client.socket.write(`name changed to ${newName}\n`);
+  pool.forEach(c => c.socket.write(`${client.nick} changed their name!`));
+});
+
+ee.on('@dm', (client, string) => {
+  let mention = string.trim().split('', 2)[1];
+  let mentionedClient = pool.filter(c => c.nick === mention)[0];
+
+  mentionedClient.socket.write(`${client.nick} says: ${string.trim().split('').slice(1).join()}\n`);
+  client.socket.write(`You sent a dm to ${mentionedClient} saying: ${string.trim().split('').slice(1).join()}\n`);
 });
 
 server.on('connection', socket => {
   let client = new Client(socket);
-  // console.log('client', client)
   pool.push(client);
-  pool.forEach(c => c.socket.write(`${client.nick} has joined the channel\n`));
+  pool.forEach(c => c.socket.write(`A wild ${client.nick} appears in the channel!`));
 
   socket.on('data', data => {
     let cmd = data.toString().split(' ').shift().trim();
     console.log(cmd);
-
-    if(cmd === '@all') {
-      console.log(data.toString().split(' ').slice(1).join() + '\n');
-      pool.forEach(c => c.socket.write(client.name + ' says ' + data.toString().split(' ').slice(1).join() + '\n'));
-
-    } else if(cmd === '@nick') {
-      let makeName = data.toString().split(' ').pop().trim();
-      //FYI, credit to Isaac. Isaac explained how this works to me//
-      client.nick = makeName;
-      console.log(client.nick);
-      pool.forEach(c => c.socket.write(`${client.nick} changed his name!`));
-
-//Pair programmed with Said//
-    } else if(cmd === '@dm') {
-      let commandLineArr = data.toString().split(' ');
-      // console.log(commandLineArr);
-      // console.log('the 1 in cLArr', commandLineArr[1]);
-      pool.forEach(client => {
-        if (commandLineArr[1] === client.nick) {
-          console.log(commandLineArr[2]);
-          client.socket.write(commandLineArr[2]);
-        }
-      });
-    }
   });
 
-  //I'm trying to figure out what to do with errors but honestly I'm not sure//
-  ee.on('error', (err) => {
+  socket.on('error', (err) => {
     if (err) console.error('Alas, alack, an error!');
-  });
-  ee.emit('error', (err) => {
-    if (err) console.error('A wild error appears!');
-    socket.destroy(); //it says end in the readme and I feel like given end can still do stuff that can't be right
   });
 });
 
-server.listen(3000, () => console.log('listening on port 3000'));
+server.listen(3000, () => console.log(`listening on port ${PORT}`));
