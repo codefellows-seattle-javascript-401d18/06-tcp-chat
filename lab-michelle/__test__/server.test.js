19c4f711481658fe0e53d8602271cedb09c0b06e
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-09-02 20:13:09 -0700

    Adjusted so tests should work properly

--- lab-michelle/__test__/server.test.js
+++ lab-michelle/__test__/server.test.js
@@ -1,3 +1,42 @@
 'use strict';
 
-const server = 
+const server = require('../server');
+const net = require('net');
+require('jest');
+
+describe('Server instance', function() {
+  //setting up our after all block so we close the server when we are done with tests//
+  afterAll(done => {
+    server.close();
+    done();
+  });
+
+  describe('new client joins chat', ()=> {
+    test('notify that a user joined', done => {
+      let client = net.connect({port: 3000}, () => {
+        client.on('data', data => {
+          expect(data.toString()).toContain('appears');
+          client.end();
+          done();
+        });
+      });
+    });
+    test('should write hellooo from client 1 to 2', done => {
+      let client1 = net.connect({port:3000}, ()=> {
+        client1.on('data', data => {
+          console.log(data.toString());
+          if(!data.toString().includes('appears')) {
+            expect(data.toString()).toMatch(/hellooo/);
+          }
+        });
+      });
+      let client2 = net.connect({port: 3000}, ()=> {
+        client2.write('@all hellooo', ()=> {
+          client1.end();
+          client2.end();
+          done();
+        });
+      });
+    });
+  });
+});
